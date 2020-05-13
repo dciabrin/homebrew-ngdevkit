6d45101f873175fcdc11b72ed82a31f42fff77a4
Damien Ciabrini <damien.ciabrini@gmail.com>
2020-05-13 19:31:36 +0200

    Nightly build ngdevkit-gngeo 202001191709

--- Formula/ngdevkit-gngeo.rb
+++ Formula/ngdevkit-gngeo.rb
@@ -5,6 +5,13 @@ class NgdevkitGngeo < Formula
   sha256 "2f2674bb84c6cddaeedb785964e7130298c3f76ef85cfb5e0f5e8ff39f8e8ea0"
   version "0.8.1+20200119170941-1"
 
+  bottle do
+    root_url "https://dl.bintray.com/dciabrin/bottles-ngdevkit"
+    sha256 "a753caed9ef708ddad0451243704af21d8b74b431a2ca045a77c52ad02a27b68" => :catalina
+    sha256 "18a39654d54f26a2e9291364c511bc032964d79b0cfb1310ddfa3aafaf971162" => :mojave
+    sha256 "b93dfae458e1c1be37d6352467bf4e7afdbc5197983bb79126c2330ea38eb349" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
