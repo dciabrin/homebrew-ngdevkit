bd303aeefb53d1ce7ff8d93a82df18fe2e6a570c
Damien Ciabrini <damien.ciabrini@gmail.com>
2020-05-16 10:22:42 +0200

    Nightly build ngdevkit-toolchain 20191223211107

--- .travis/publish-bottle.sh
+++ .travis/publish-bottle.sh
@@ -5,4 +5,4 @@ PKG=$1
 PKG_VERSION=$(sed -ne 's/.*version.*"\([^"]*\)".*/\1/p' Formula/$PKG.rb | head -1)
 curl -s -X POST -u dciabrin:$BINTRAY_API_KEY "https://api.bintray.com/content/dciabrin/bottles-ngdevkit/$PKG/${PKG_VERSION}/publish"
 echo
-echo "All bottles for $PKG compiled and published, branch $TRAVIS_BRANCH is ready to be merged in master"
+echo "All bottles for $PKG compiled and published"
--- Formula/ngdevkit-toolchain.rb
+++ Formula/ngdevkit-toolchain.rb
@@ -5,6 +5,13 @@ class NgdevkitToolchain < Formula
   sha256 "99d0c966f5810eda6a83d0f13ea650e89c412c74dd40a9fd409cf061f6c35c95"
   version "0.1+20191223211107-1"
 
+  bottle do
+    root_url "https://dl.bintray.com/dciabrin/bottles-ngdevkit"
+    sha256 "8467d86c6b783a7a33fd9d761172862833f8bb80b76e42ec12bb2555f663de46" => :catalina
+    sha256 "3388a6c6274bdc9375e99bf82099a5985e29f82288d49181e10de998415bc979" => :mojave
+    sha256 "27b73d604d45694994110f3af373ee82336d0cb966c011ca53e4a86af5a4318a" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
@@ -29,8 +36,15 @@ class NgdevkitToolchain < Formula
   def install
     $gmake = "#{Formula["make"].opt_bin}/gmake"
     ENV["MAKE"] = $gmake
-    system $gmake, "--", "prefix=#{prefix}"
-    system $gmake, "install", "--", "prefix=#{prefix}"
+    $silent_flags = "-fno-show-source-location -fno-caret-diagnostics -fno-color-diagnostics -fno-elide-type -fno-diagnostics-fixit-info -Wno-array-bounds -Wno-unused-command-line-argument"
+    ENV["CFLAGS"] = $silent_flags
+    ENV["CXXFLAGS"] = $silent_flags
+    ENV["CPPFLAGS"] = $silent_flags
+    # travis cut job when they are too verbose, so cut
+    # the output drastically until we find a better
+    # way to limit the compilation logs
+    system $gmake + " -- prefix=#{prefix} | awk '{printf \".\"; fflush()}'"
+    system $gmake + " install -- prefix=#{prefix}"
   end
 
   test do
