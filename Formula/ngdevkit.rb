d06d0af6f79b7225dd091047b0f755f2f4b5a462
Damien Ciabrini <damien.ciabrini@gmail.com>
2021-05-09 19:23:17 +0200

    Disable bintray bottles for all ngdevkit subprojects

--- Formula/ngdevkit.rb
+++ Formula/ngdevkit.rb
@@ -5,12 +5,6 @@ class Ngdevkit < Formula
   version "0.2+202103252051-1"
   sha256 "cb96204ef85055a3ed56664ec723b321ff98d68da1245c7968c24484c966861e"
 
-  bottle do
-    root_url "https://dl.bintray.com/dciabrin/bottles-ngdevkit"
-    sha256 cellar: :any_skip_relocation, catalina: "d0088168f5b29bbb9759561361d1b1a646f342c45101519426b91db5499dc1e0"
-    sha256 cellar: :any_skip_relocation, mojave:   "714dc1992e87d5a515dd6799065522f1cae2260e232b7174ea030d03ada573ca"
-  end
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "make" => :build
