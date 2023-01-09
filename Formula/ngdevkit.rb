20809a2ca8f0a606ea2b3332b6aa3e1f1b362eb3
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-01-09 19:52:51 +0100

    Nightly rebuild ngdevkit 202212301452

--- Formula/ngdevkit.rb
+++ Formula/ngdevkit.rb
@@ -2,14 +2,8 @@ class Ngdevkit < Formula
   desc "Open source development for Neo-Geo"
   homepage "https://github.com/dciabrin/ngdevkit"
   url "https://github.com/dciabrin/ngdevkit/archive/nightly-202212301452.tar.gz"
-  version "0.2+202212301452-2"
+  version "0.2+202212301452-3"
   sha256 "0c52639be5276931b9f3f6ccc98b229606947d21fe50cde06e4c1740a74427e1"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/ngdevkit-0.2+202212301452-2"
-    sha256 cellar: :any_skip_relocation, monterey: "6bd1f27f1439d2e4536ed5f92e30cb45390216f454c41631e949dce21b6a4098"
-    sha256 cellar: :any_skip_relocation, big_sur:  "ce14a4467879185f0ed013abc551e9e28b42f77e6f3cb73c4b75842fc6d69801"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
