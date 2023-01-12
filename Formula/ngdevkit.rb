edd6c05b9b4cb9fd4d6b5ddd1112920126b2b652
runner <runner@Mac-1673542136840.local>
2023-01-12 16:57:19 +0000

    Nightly rebuild ngdevkit 202212301452 (bottled)

--- Formula/ngdevkit.rb
+++ Formula/ngdevkit.rb
@@ -5,6 +5,12 @@ class Ngdevkit < Formula
   version "0.2+202212301452-4"
   sha256 "0c52639be5276931b9f3f6ccc98b229606947d21fe50cde06e4c1740a74427e1"
 
+  bottle do
+    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/ngdevkit-0.2+202212301452-4"
+    sha256 cellar: :any_skip_relocation, monterey: "b9c827db4a3045cb8189808b4db776d5746b8b530b477ac85629a012e50decac"
+    sha256 cellar: :any_skip_relocation, big_sur:  "f1198c0cf31b3270a118d06b7cf35c24a6c748923efda2189e9c243d11d4becd"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "make" => :build
