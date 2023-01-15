a0ab36979817b9ff98d0087f8fd29839598192ff
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-01-15 19:17:37 +0100

    New script to GC old nightly releases
    Ref dciabrin/ngdevkit#84

---
+++ .ci/gc-nightly-tags.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# Copyright (c) 2020-2023 Damien Ciabrini
+# This file is part of ngdevkit
+
+set -u
+
+# Disable verbose to prevent leaking credentials
+set +x
+
+
+help() {
+    echo "Usage: $0 --package {ngdevkit} --token {github-api-token}" >&2
+    exit ${1:-0}
+}
+
+error() {
+    echo "Error: $1" >&2
+    help 1
+}
+
+check() {
+    if [ $2 != 200 ] && [ $2 != 204 ]; then
+        error "unexpected return from '$1' ($2). Aborting"
+    fi
+}
+
+cleanup() {
+    if [ -f "${releases:-}" ]; then
+        echo "Removing temporary files"
+        rm -f "$releases"
+    fi
+}
+trap cleanup EXIT
+
+
+# ----------------- options parsing -----------------
+
+GITHUB_TOKEN=${GH_TOKEN:-}
+DRYRUN=
+
+OPTS=$(/usr/bin/getopt -n $0 --long help,dry-run,repo:,token: -- $0 $@)
+if [ $? != 0 ]; then
+    error "parsing arguments failed"
+fi
+
+eval set -- "$OPTS"
+while true; do
+    case "$1" in
+        --help) help;;
+        --dry-run ) DRYRUN=1; shift ;;
+        --repo ) PACKAGE="$2"; shift 2 ;;
+        --token ) GITHUB_TOKEN="$2"; shift 2 ;;
+        -- ) shift; break ;;
+        * ) break ;;
+    esac
+done
+
+if [ -z "${PACKAGE:-}" ]; then
+    error "no package specified"
+fi
+if [ -z "${GITHUB_TOKEN:-}" ]; then
+    error "no GitHub API credentials specified"
+fi
+CREDS=dciabrin:$GITHUB_TOKEN
+
+
+# ----------------- garbage-collect nightly releases for a specific package -----------------
+
+releases=$(mktemp -t releases-XXXXXXX.json)
+
+
+remove_release () {
+    local name=$1
+    local release_id=$(jq -r 'map(select(.name == "'"$name"'")) | .[].id' $releases)
+    local tag_name=$(jq -r 'map(select(.name == "'"$name"'")) | .[].tag_name' $releases)
+
+    echo "  removing release $release_id"
+    if [ -z "$DRYRUN" ]; then
+        ret=$(curl -s -w "%{http_code}" -X DELETE -u $CREDS https://api.github.com/repos/dciabrin/homebrew-ngdevkit/releases/${release_id})
+        check "removing release_id $release_id" $ret
+        sleep 0.7
+    fi
+
+    echo "  removing associated tag $tag_name"
+    if [ -z "$DRYRUN" ]; then
+        ret=$(curl -s -w "%{http_code}" -X DELETE -u $CREDS https://api.github.com/repos/dciabrin/homebrew-ngdevkit/git/refs/tags/${tag_name})
+        check "removing tag_name $tag_name" $ret
+        sleep 0.7
+    fi
+}
+
+
+echo "Retrieving releases for package $PACKAGE..."
+ret=$(curl -s -w "%{http_code}" -X GET -u $CREDS https://api.github.com/repos/dciabrin/homebrew-ngdevkit/releases -o $releases)
+check "retrieving releases" $ret
+
+# Note: keep the two most recent nightly releases
+# to ease transition to newer releases
+names=$(jq -r '. | map(select(.name | sub("^(?<pkg>[^0-9]*)-.*"; "\(.pkg)") | test("^'"$PACKAGE"'$")) ) | sort_by(.updated_at) | .[] | .name' $releases)
+echo $names
+
+echo "Processing all releases found for package $PACKAGE..."
+num=0
+for n in $names; do
+    echo "release $n"
+    if [ $num -ge 2 ]; then
+        remove_release $n
+    fi
+    ((num++))
+done
+
+echo
+if [ $num -gt 2 ]; then
+    echo "$(expr $num - 2) release(s) removed for package $PACKAGE"
+else
+    echo "No release removed for package $PACKAGE"
+fi
