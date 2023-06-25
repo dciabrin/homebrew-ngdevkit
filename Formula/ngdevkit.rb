016b1c729723425ab3bae9ff5f73667cb62ca8af
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-06-25 21:25:20 +0200

    Nightly version ngdevkit 0.2+202306062054-2

--- Formula/ngdevkit.rb
+++ Formula/ngdevkit.rb
@@ -2,14 +2,8 @@ class Ngdevkit < Formula
   desc "Open source development for Neo-Geo"
   homepage "https://github.com/dciabrin/ngdevkit"
   url "https://github.com/dciabrin/ngdevkit/archive/nightly-202306062054.tar.gz"
-  version "0.2+202306062054-1"
+  version "0.2+202306062054-2"
   sha256 "c7b0454ad40680f61cee19e6c6885114a042d7b3f5fa8ed34a95c58b65673588"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/ngdevkit-0.2+202306062054-1"
-    sha256 cellar: :any_skip_relocation, monterey: "742445f5870ba43cd238458e268c1d70b20f6c3955fe85b881081b1212f3ce75"
-    sha256 cellar: :any_skip_relocation, big_sur:  "e662fc23d77cf40a2103c69c2b2dece11137d447ab6542ba739c9d5894ee6ddf"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
