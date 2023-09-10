ad0f9308b14e8d02bfe490a5ced17bb7359f70a5
runner <runner@Mac-1694366359121.local>
2023-09-10 21:01:01 +0000

    Nightly rebuild emudbg 202305151016 (bottled)

--- Formula/emudbg.rb
+++ Formula/emudbg.rb
@@ -5,6 +5,12 @@ class Emudbg < Formula
   version "0.2+202305151016-2"
   sha256 "49a294ac41194559f6a59271fa9a15d457833e2ebc4bb3ed86121d567f89d4e9"
 
+  bottle do
+    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/emudbg-0.2+202305151016-2"
+    sha256 cellar: :any_skip_relocation, monterey: "14177809a8dfcd9887a18a79337066e8fd2837ed1cc0b99216c54c5348138a60"
+    sha256 cellar: :any_skip_relocation, big_sur:  "4f6d59e7c1010ec6ea65cf573e87c37ad0f7096cba52d7e13e7edc68c637d3d4"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
