70fb9793099015f400d73667239762d437cf8987
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-09-11 08:20:09 +0200

    Nightly rebuild ngdevkit-gngeo 202305160925

--- Formula/ngdevkit-gngeo.rb
+++ Formula/ngdevkit-gngeo.rb
@@ -2,14 +2,8 @@ class NgdevkitGngeo < Formula
   desc "Portable Neo-Geo emulator customized for ngdevkit"
   homepage "https://github.com/dciabrin/gngeo"
   url "https://github.com/dciabrin/gngeo/archive/nightly-202305160925.tar.gz"
-  version "0.8.1+202305160925-1"
+  version "0.8.1+202305160925-2"
   sha256 "d79b6e1279f54cf0efa04f2c0698b16e46a2d0ff499c29856bcf9f49350412fa"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/ngdevkit-gngeo-0.8.1+202305160925-1"
-    sha256 monterey: "866fff959eb3921e56d13a0ea04f03bd5a52fb2045f0c70389fa93aef483a858"
-    sha256 big_sur:  "5affc616c0f1cb1e190ec226f295cf745a46baf01ea1178518689f5c2a7a02b7"
-  end
 
   depends_on "autoconf" => :build
   depends_on "autoconf-archive" => :build
