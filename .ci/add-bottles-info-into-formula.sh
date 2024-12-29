35c61fb4455fe864e13d064453cc58fa2fd28383
Damien Ciabrini <damien.ciabrini@gmail.com>
2024-12-29 19:17:56 +0100

    Nightly version emudbg 0.2+202403192028-2

---
+++ .ci/add-bottles-info-into-formula.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ue
+
+# how many lines in the final diff chunk
+lines=$(grep -h -e '^+ ' bottle-*.diff | sort -u | wc -l | tr -d ' ')
+
+# construct the new diff with inlined bottle info and updated patch metadata
+awk -F'[, +-]' '
+/@@/     {print "@@ -"$3","$4" +"$3","($4+'${lines}'+1)" @@"; next}
+/cellar/ { system("grep -h cellar bottle-*.diff"); next}
+         {print $0}
+' $(ls -1 bottle-*.diff | head -1) > all-bottles.diff
+
+# update the target formula
+patch -p1 < all-bottles.diff
+
+echo "bottles data added to formula:"
+cat all-bottles.diff
--- Formula/emudbg.rb
+++ Formula/emudbg.rb
@@ -2,13 +2,8 @@ class Emudbg < Formula
   desc "Emulator-agnostic source-level debugging API"
   homepage "https://github.com/dciabrin/emudbg"
   url "https://github.com/dciabrin/emudbg/archive/refs/tags/nightly-202403192028.tar.gz"
-  version "0.2+202403192028-1"
+  version "0.2+202403192028-2"
   sha256 "56306dd13bebcf001bcf340695b05d225bf077fc9ef99283db8f46eaf019e536"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/emudbg-0.2+202403192028-1"
-    sha256 cellar: :any_skip_relocation, monterey: "34be83e2846c598370ee3d145bf24f180d939945df02d13f59db0c0d427ef3d6"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
