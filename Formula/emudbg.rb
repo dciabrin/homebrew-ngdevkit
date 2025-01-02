8dc0ef62ff24158ce86a5f130619751eaf52f2bb
Damien Ciabrini <damien.ciabrini@gmail.com>
2025-01-02 09:59:14 +0100

    Nightly version emudbg 0.2+202403192028-3

--- Formula/emudbg.rb
+++ Formula/emudbg.rb
@@ -2,14 +2,8 @@ class Emudbg < Formula
   desc "Emulator-agnostic source-level debugging API"
   homepage "https://github.com/dciabrin/emudbg"
   url "https://github.com/dciabrin/emudbg/archive/refs/tags/nightly-202403192028.tar.gz"
-  version "0.2+202403192028-2"
+  version "0.2+202403192028-3"
   sha256 "56306dd13bebcf001bcf340695b05d225bf077fc9ef99283db8f46eaf019e536"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/emudbg-0.2+202403192028-2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d003f50a34a4d82c1762c1014974ce9bb358f7340cfb3ac03095df20a83221e8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "631bb2078f40b2d23c9f9a4b6858e6802f6ca4a4bdcebc6d30710e6fb91d22fa"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
