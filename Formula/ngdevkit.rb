0d7fb9d28c9bd5971202c9c0b790fbffaf18650a
Damien Ciabrini <damien.ciabrini@gmail.com>
2025-03-15 16:14:12 +0100

    Nightly version ngdevkit 0.4+202502261828-2
    Fix macOS brew build that require homebrew's M4 instead of the
    system's M4 as configured during build
    
    ref ngdevkit/114

--- Formula/ngdevkit.rb
+++ Formula/ngdevkit.rb
@@ -2,16 +2,12 @@ class Ngdevkit < Formula
   desc "Open source development for Neo-Geo"
   homepage "https://github.com/dciabrin/ngdevkit"
   url "https://github.com/dciabrin/ngdevkit/archive/refs/tags/nightly-202502261828.tar.gz"
-  version "0.4+202502261828-1"
+  version "0.4+202502261828-2"
   sha256 "6afaa56caa0ed84d944feb38dd69823954fde35ee485961c1bd27bea89ff747b"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/ngdevkit-0.4+202502261828-1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "221c08aa3dc41e3c4125f3fa252e4fb7007614a18768d4220b566b74cda8a031"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "m4" => :build
   depends_on "make" => :build
   depends_on "zip" => :build
   depends_on "ngdevkit-toolchain"
@@ -22,6 +18,10 @@ class Ngdevkit < Formula
     # We require gnu make > 4.0, so use the one from brew
     gmake = "#{Formula["make"].opt_bin}/gmake"
     ENV["MAKE"] = gmake
+
+    # Homebrew picks the system's gnu M4, which is too old for autoreconf
+    # (it doesn't have option --gnu). Pick homebrew's version instead
+    ENV["M4"] = "#{Formula["m4"].opt_bin}/m4"
 
     # For M1 macs, brew libraries are installed in a non-standard
     # location, so some env vars must be set for autotools
