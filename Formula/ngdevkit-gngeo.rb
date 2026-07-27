2de380652cb066327721a94a10ab98c734e63a15
Damien Ciabrini <damien.ciabrini@gmail.com>
2026-07-28 00:04:14 +0200

    Nightly version ngdevkit-gngeo 0.8.1+202607271849-2

--- Formula/ngdevkit-gngeo.rb
+++ Formula/ngdevkit-gngeo.rb
@@ -2,14 +2,8 @@ class NgdevkitGngeo < Formula
   desc "Portable Neo-Geo emulator customized for ngdevkit"
   homepage "https://github.com/dciabrin/gngeo"
   url "https://github.com/dciabrin/gngeo/archive/refs/tags/nightly-202607271849.tar.gz"
-  version "0.8.1+202607271849-1"
+  version "0.8.1+202607271849-2"
   sha256 "230e198e27e5f9ffc683a3f85097403c27110609879e2a84fa43c23472b8c3a2"
-
-  bottle do
-    root_url "https://github.com/dciabrin/homebrew-ngdevkit/releases/download/ngdevkit-gngeo-0.8.1+202607271849-1"
-    sha256 arm64_sequoia: "4e38344b7d4e9df930e23230a6de972eb51e47f67a5eb9f0b5cb3015bbb937f5"
-    sha256 arm64_sonoma:  "e6225d70aba4a82a62f994bea9208979f8053839c2d98106dd268de73b47f25b"
-  end
 
   depends_on "autoconf" => :build
   depends_on "autoconf-archive" => :build
@@ -19,6 +13,7 @@ class NgdevkitGngeo < Formula
   depends_on "pkg-config" => :build
   depends_on "glew"
   depends_on "sdl2"
+  depends_on "sdl2-compat"
 
   def install
     # We require gnu make > 4.0, so use the one from brew
